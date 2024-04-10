0f461f6ad022b738bb2d89dbf1a2a18dc9990697
ElvineJoy <elvinkuiyungest@gmail.com>
2024-04-10 23:00:16 +0300

    the do and while looplab completed.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+// for loop code along
+// for( let age =30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy Birthday to me!`);
+    
+// };
+
+// const gifts =['teddy bear', 'toy car', 'doll'];
+
+// function wrappedGifts(gifts) {
+// for(let i = 0; i < gifts.length; i++) {
+//     console.log(`wrapped ${gifts[i]} and added a bow!`);
+// }
+// return gifts;
+// }
+// wrappedGifts(gifts)
+
+//  for loop assignment 
+function writeCards(people,msg) {
+    const thanksArray = [];
+ for(let i= 0; i < people.length; i++) {
+    let message = `Thank you, ${people[i]}, for the wonderful ${msg} gift!`;
+    thanksArray.push(message);
+ }
+return thanksArray;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"],'surprise');
+
+// while loop assignemnt
+function countDown(num) {
+    while (num >= 0 ) {
+        console.log(num--);       
+    }
+    return num;
+}
+countDown(10);
+
